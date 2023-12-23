c0ca79cbdd9426c2df8a982ad1104bcf3bd51a7a
Tiago Souza <tiago0214@gmail.com>
2023-12-23 16:56:32 -0300

    att

--- ExemploExplorando/Program.cs
+++ ExemploExplorando/Program.cs
@@ -1,13 +1,88 @@
 ﻿using ExemploExplorando.Models;
+using System.Globalization;
 
 
-decimal valorMonetario = 82.40M;
+string dataString = "30/12/2023 14:30";
+DateTime.TryParseExact(dataString, "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime data);
+// esse metodo vai tentar fazer o parse da minha string, parametros: 1:string, 2: formato, 3: variante de cultura(independe da cultura, porque eu já defini o formato), 4: sem estilo, mesma coisa do parametro anterior, porque eu já defini o formato que eu espero, 5: se ele conseguir converter ou não, ele vai jogar na minha variavel chamada data.
+//com essa conversão, diferente da outra, se der erro, ele não da erro no meu programa.
 
-Console.WriteLine($"{valorMonetario:c}");
-//quiser fazer um valor monetário, eu preciso colocar o template string, e depois uso :C
-//quando eu utilizo o : eu estou dizendo que quero formatar o meu valor, e depois eu coloco para oque eu quero formatar. :C currency
+//agora eu vou para um exemplo, em que eu estou esperando receber uma data, mas provavelmente ela vai vir como string, então eu tenho que converte-lá.
+Console.WriteLine(data);
 
 
+
+
+
+
+
+// DateTime data = DateTime.Now;
+
+
+// Console.WriteLine(data.ToShortDateString());
+// Console.WriteLine(data.ToShortTimeString());
+
+// Console.WriteLine(data);
+// Console.WriteLine(data.ToString("dd/MM/yyyy HH:mm"));
+//se atentar que se eu colocar o mm minusculo na data, eu estou representando os minutos. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+// //para mudar a localização do meu programa.
+
+
+// decimal valorMonetario = 1282.40M;
+
+// Console.WriteLine(valorMonetario.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+// //posso travar a localização do meu sistema e depois alterar a representação de algo em alguma cultura especifica.
+
+// Console.WriteLine($"{valorMonetario:C}");
+// //quiser fazer um valor monetário, eu preciso colocar o template string, e depois uso :C
+// //quando eu utilizo o : eu estou dizendo que quero formatar o meu valor, e depois eu coloco para oque eu quero formatar. :C currency
+
+
+// //diversas maneiras que eu posso formatar e representar um numero.
+// //Console.WriteLine($"{valorMonetario.ToString("C2")}"); C: significa moeda, e 2 significa o numero de casas decimais.
+
+
+// double porcetagem = .3442;
+// Console.WriteLine(porcetagem.ToString("P"));
+
+// int numero = 123456;
+// Console.WriteLine(numero.ToString("##/##/##"));
 
 
 
